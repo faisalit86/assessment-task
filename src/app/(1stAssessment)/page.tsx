8cf56115df43e265e77dc069a7e33ffa16bb79cb
ccriptdev8 <mfaisalit86@gmail.com>
2025-03-06 17:05:01 -0800

    remove : removed unused code

--- src/app/(1stAssessment)/page.tsx
+++ src/app/(1stAssessment)/page.tsx
@@ -1,12 +1,6 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import FeaturedBlog from "./components/FeaturedBlog";
-import { Card } from "@/components/ui/card";
-import { Eye } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
-import { fetchBlogs } from "@/lib/api-calls";
 import BlogPosts from "./components/BlogPosts";
+import FeaturedBlog from "./components/FeaturedBlog";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
