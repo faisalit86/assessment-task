147dc6a4295f46258af20ecab70c0aea5966be87
ccriptdev8 <mfaisalit86@gmail.com>
2025-03-06 17:20:14 -0800

    fix : fixing build errors

--- src/app/(1stAssessment)/page.tsx
+++ src/app/(1stAssessment)/page.tsx
@@ -2,7 +2,7 @@ import BlogPosts from "./components/BlogPosts";
 import FeaturedBlog from "./components/FeaturedBlog";
 import HeroSection from "./components/HeroSection";
 
-export default function Home() {
+export default function AssessmentPage() {
   return (
     <div className="">
       <HeroSection />
